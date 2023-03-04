d0137365ed129599330e893429fe0a3f8324a7c8
Mohammad Mahdi Mottaghi <mottaghimohammadmahdi@gmail.com>
2023-03-04 18:31:06 +0330

    create template files

---
+++ ToDo App/main.js
@@ -0,0 +1,7 @@
+let body = document.querySelector('body');
+let h1 = document.createElement('h1');
+h1.textContent = "Hello"
+
+body.append(h1);
+
+console.log(body);
---
+++ ToDo App/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.js", "./src/style/index.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
